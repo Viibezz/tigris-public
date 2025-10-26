1864de3bab7c01f6374cd8657cc5d82ec23825d8
Faisal Bahoo <brand@live.com>
2025-10-25 22:38:05 -0700

    fix: shell script syntax

--- build_utils/build.sh
+++ build_utils/build.sh
@@ -3,9 +3,8 @@
 # Environment variables for different build targets
 dev() {
     # export URL="http://192.168.1.81:8000/"
-    # export URL="https://viibezz.github.io/tigris-public/"
+    export URL="https://viibezz.github.io/tigris-public/"
 }
-
 
 # Set environment based on argument
 if [ "$1" == "dev" ]; then
