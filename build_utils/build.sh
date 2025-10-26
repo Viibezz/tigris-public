1f91036848635b1336f88ce5b74eb47d249166e8
Faisal Bahoo <brand@live.com>
2025-10-25 22:45:19 -0700

    include protocol in url

--- build_utils/build.sh
+++ build_utils/build.sh
@@ -2,8 +2,8 @@
 
 # Environment variables for different build targets
 dev() {
-    # export URL="http://192.168.1.81:8000/"
-    export URL="https://viibezz.github.io/tigris-public/"
+    export URL="http://192.168.1.81:8000/"
+    # export URL="https://viibezz.github.io/tigris-public/"
 }
 
 # Set environment based on argument
