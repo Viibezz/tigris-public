2f2e6474b0e6a6e7eb4cba436ad1bf79abaf6935
Faisal Bahoo <brand@live.com>
2025-10-25 22:35:34 -0700

    refactor build script

--- build_utils/build.sh
+++ build_utils/build.sh
@@ -2,21 +2,14 @@
 
 # Environment variables for different build targets
 dev() {
-    export URL="http://192.168.1.81:8000/"
+    # export URL="http://192.168.1.81:8000/"
+    # export URL="https://viibezz.github.io/tigris-public/"
 }
 
-prod() {
-    export URL="https://viibezz.github.io/tigris-public/"
-}
 
 # Set environment based on argument
 if [ "$1" == "dev" ]; then
     dev
-elif [ "$1" == "prod" ]; then
-    prod
-else
-    echo "Usage: $0 {dev|prod}"
-    exit 1
 fi
 
 # This script builds the project using python
